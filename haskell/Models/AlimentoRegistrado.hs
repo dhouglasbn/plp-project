52c8f3c26666265a950d81caa22bccffbf10130e
jgluiggi <jgluiggi@protonmail.com>
2023-09-21 12:59:40 -0300

    AlimentoRegistrado refatorado levemente.

--- haskell/Models/AlimentoRegistrado.hs
+++ haskell/Models/AlimentoRegistrado.hs
@@ -18,12 +18,14 @@ calculaCarbos comida = carboidratos (alimento comida) * quantia comida
 calculaGorduras :: AlimentoRegistrado -> Float
 calculaGorduras comida = gorduras (alimento comida) * quantia comida
 
+--        totalProteinas  = sum [proteinas (alimento registrado) * quantia registrado | registrado <- registrados]
+
 valorTotal :: [AlimentoRegistrado] -> (Float, Float, Float, Float)
 valorTotal registrados =
-    let totalKcal       = sum [kcal (alimento registrado) * quantia registrado | registrado <- registrados]
-        totalProteinas  = sum [proteinas (alimento registrado) * quantia registrado | registrado <- registrados]
-        totalCarbos     = sum [carboidratos (alimento registrado) * quantia registrado | registrado <- registrados]
-        totalGorduras   = sum [gorduras (alimento registrado) * quantia registrado | registrado <- registrados]
+    let totalKcal       = sum [calculaCalorias registrado | registrado <- registrados]
+        totalProteinas  = sum [calculaProteinas registrado | registrado <- registrados]
+        totalCarbos     = sum [calculaCarbos registrado | registrado <- registrados]
+        totalGorduras   = sum [calculaGorduras registrado | registrado <- registrados]
     in (totalKcal, totalProteinas, totalCarbos, totalGorduras)
 
 valorDasRefeicoes :: [AlimentoRegistrado] -> [AlimentoRegistrado] -> [AlimentoRegistrado] -> [AlimentoRegistrado] -> (Float, Float, Float, Float)
