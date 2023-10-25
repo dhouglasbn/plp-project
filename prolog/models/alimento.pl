c0e12cad871f631dd3a7e2ea3d34057efb59aa42
jgluiggi <jgluiggi@protonmail.com>
2023-10-24 22:00:42 -0300

    Banco de dados restruturado e model de alimento feito.

--- prolog/models/alimento.pl
+++ prolog/models/alimento.pl
@@ -1,5 +1,20 @@
 module(alimento, [
-
+	alimento/7,
+	alimento_set_quantidade/3
 	]).
 
 alimento
+
+alimento(ID, Nome, Kcal, Proteinas, Gorduras, Carboidratos, Quantidade):-
+	string_lower(Nome, NomeMinusculo),
+	Alimento = (ID|NomeMinusculo|Kcal|Proteinas|Gorduras|
+		Carboidratos|Quantidade).
+
+alimento_set_quantidade(Alimento, Quantidade, NovoAlimento):-
+	(A|B|Kcal|Prot|Gord|Carbo|Dados|_) = Alimento,
+	NovaKcal = Kcal * Quantidade,
+	NovaProt = Prot * Quantidade,
+	NovaGord = Gord * Quantidade,
+	NovoCarbo = Carbo * Quantidade,
+	NovoAlimento = (A|B|NovaKcal|NovaProt|NovaGord|NovoCarbo
+		|Quantidade).
