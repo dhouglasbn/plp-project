7d3f65b9e67edaeb8eccd05c5edb243cf5374c52
Caio Victor <caio.araujo@ccc.ufcg.edu.br>
2023-10-24 22:52:29 -0300

    commit.

--- prolog/main.pl
+++ prolog/main.pl
@@ -30,25 +30,29 @@ login(_) :-
 criar_conta() :-
     writeln("Digite seu nome: "),
     read(Nome),
-    writeln("Digite a senha: "),
+    writeln("Digite sua senha: "),
     read(Senha),
-    writeln("Escolha seu gênero (m ou f): "),
+    writeln("Escolha seu gênero (m)asculino ou (f)eminino: "),
     read(Genero),
-    writeln("Digite a idade: "),
+    writeln("Digite sua idade: "),
     read(Idade),
-    writeln("Digite o peso: "),
+    writeln("Digite seu peso: "),
     read(Peso),
-    writeln("Digite a altura: "),
+    writeln("Digite sua altura: "),
     read(Altura),
-    writeln("Digite seu objetivo de peso: "),
+    writeln("Digite sua meta de peso: "),
     read(Meta),
 
-    % print(Genero),
-    % genero_valido(Genero),
-    % idade_valida(IdadeStr),
-    % peso_valido(PesoStr),
-    % altura_valida(AlturaStr),
-    % meta_valida(MetaStr),
+    read_integer(IdadeStr, Idade),
+    read_float(PesoStr, Peso),
+    read_float(AlturaStr, Altura),
+    read_float(MetaStr, Meta),
+
+    genero_valido(Genero),
+    idade_valida(Idade),
+    peso_valido(Peso),
+    altura_valida(Altura),
+    meta_valida(Meta),
 
     usuario(Senha, Nome, Genero, Idade, Peso, Altura, Meta, Usuario),
 
@@ -56,46 +60,42 @@ criar_conta() :-
     main()
     .
 
-% genero_valido("m") :- !.
-% genero_valido("f") :- !.
-% genero_valido(_) :-
-%     writeln("Gênero inválido. Use 'M' para masculino ou 'F' para feminino."),
-%     fail.
+genero_valido("m") :- !.
+genero_valido("f") :- !.
+genero_valido(_) :-
+    writeln("Gênero inválido. Use 'M' para masculino ou 'F' para feminino."),
+    fail.
 
-% idade_valida(IdadeStr) :-
-%     read_integer(IdadeStr, Idade),
-%     Idade > 0,
-%     Idade < 100, !.
-% idade_valida(_) :-
-%     writeln("Idade inválida. Deve ser um número entre 1 e 99."),
-%     fail.
+idade_valida(Idade) :-
+    Idade > 0,
+    Idade < 100, !.
+idade_valida(_) :-
+    writeln("Idade inválida. Deve ser um número entre 1 e 99."),
+    fail.
 
-% peso_valido(PesoStr) :-
-%     read_float(PesoStr, Peso),
-%     Peso > 0, !.
-% peso_valido(_) :-
-%     writeln("Peso inválido. Deve ser um número positivo."),
-%     fail.
+peso_valido(Peso) :-
+    Peso > 0.0, !.
+peso_valido(_) :-
+    writeln("Peso inválido. Deve ser um número positivo."),
+    fail.
 
-% altura_valida(AlturaStr) :-
-%   read_float(AlturaStr, Altura),
-% Altura > 0.0, !.
-% altura_valida(_) :-
-%     writeln("Altura inválida. Deve ser um número positivo."),
-%     fail.
+altura_valida(Altura) :-
+    Altura > 0.0, !.
+altura_valida(_) :-
+    writeln("Altura inválida. Deve ser um número positivo."),
+    fail.
 
-% meta_valida(MetaStr) :-
-%     read_float(MetaStr, Meta),
-%     Meta > 0.0, !.
-% meta_valida(_) :-
-%     writeln("Meta inválida. Deve ser um número positivo."),
-%     fail.
+meta_valida(Meta) :-
+    Meta > 0.0, !.
+meta_valida(_) :-
+    writeln("Meta inválida. Deve ser um número positivo."),
+    fail.
 
-% read_integer(Str, Int) :-
-%     catch(atom_number(Str, Int), _Error, fail).
+read_integer(Str, Int) :-
+    catch(atom_number(Str, Int), _Error, fail).
 
-% read_float(Str, Float) :-
-%     catch(atom_number(Str, Float), _Error, fail).
+read_float(Str, Float) :-
+    catch(atom_number(Str, Float), _Error, fail).
 
 
 
@@ -126,7 +126,17 @@ menu(Usuario) :-
     (Opcao = 1 ->
         mini_menu_configuracao(Usuario)
     ; Opcao = 2 ->
-        calcular_progresso_calorico(Usuario)
+        writeln(Usuario),
+        calcular_progresso_calorico(Usuario, Kfinal),
+        print(Kfinal),
+        metaProteins is (Kfinal * 0.25) / 4.0,
+        print(metaProteins),
+        metaLipids is (Kfinal * 0.25) / 9.0,
+        print(metaLipids),
+        metaCarbohydrates is (Kfinal * 0.5) / 4.0,
+        print(metaCarbohydrates),
+        menu(Usuario)
+
     ; Opcao = 3 ->
         mini_menu_refeicoes(Usuario)
     ; Opcao = 4 ->
@@ -139,29 +149,6 @@ menu(Usuario) :-
     ).
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 mini_menu_configuracao(Usuario):-
     writeln("Escolha que tipo de dado voce deseja modificar:\n"),
     writeln("1 - Meta"),
@@ -171,17 +158,80 @@ mini_menu_configuracao(Usuario):-
     read(Opcao),
 
     (Opcao = 1 ->
-        atualizar_meta(Usuario)
+        writeln("Qual sua nova meta de peso?:\n"),
+        read(MetaStr),
+        read_float(MetaStr, Meta_Peso),
+        usuario_set_meta_peso(Usuario, Meta_Peso, NovoUsuario),
+        menu(NovoUsuario)
     ; Opcao = 2 ->
-        atualizar_peso(Usuario)
+        writeln("Qual seu novo peso?:\n"),
+        read(PesoStr),
+        read_float(PesoStr, Peso),
+        usuario_set_peso(Usuario, Peso, NovoUsuario),
+        menu(NovoUsuario)
     ; Opcao = 3 ->
-        atualizar_idade(Usuario)
+        writeln("Qual é a sua idade?:\n"),
+        read(IdadeStr),
+        read_integer(IdadeStr, Idade),
+        usuario_set_idade(Usuario, Idade, NovoUsuario),
+        menu(NovoUsuario)
     ; Opcao = 4 ->
         menu(Usuario)
     ; 
         writeln("Opção inválida!\n"),
         mini_menu_configuracao(Usuario)
     ).
+
+calcular_progresso_calorico(Usuario, Kfinal):-
+    usuario_get_genero(Usuario, Genero),
+    usuario_get_peso(Usuario, Peso),
+    usuario_get_altura(Usuario, Altura),
+    usuario_get_meta_peso(Usuario, Meta),
+    caloriasManterPeso(Genero, Peso, Altura, Calorias),
+
+    (Peso > Meta -> 
+    caloriasDiariasPerderPeso(Calorias, Peso, MetaPeso, Kcal),
+    Kfinal is Kcal
+    ;
+    Peso < Meta -> 
+    caloriasDiariasGanharPeso(Calorias, Peso, MetaPeso, Kcal),
+    Kfinal is Kcal
+    ;
+    Kfinal is Calorias).
+
+caloriasManterPeso("m", Peso, Altura, Calorias) :-
+    Calorias is 888.362 + (13.397 * Peso) + (4.799 * Altura).
+
+caloriasManterPeso("f", Peso, Altura, Calorias) :-
+    Calorias is 447.593 + (9.247 * Peso) + (3.098 * Altura).
+
+caloriasManterPeso(_, _, 0.0).
+
+caloriasDiariasPerderPeso(KPadrao, Peso, MetaPeso, Calorias) :-
+    Calorias is KPadrao - (25 * (Peso - MetaPeso)).
+
+caloriasDiariasGanharPeso(KPadrao, Peso, MetaPeso, Calorias) :-
+    Calorias is KPadrao + (25 * (MetaPeso - Peso)).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -382,4 +432,3 @@ submenu_exercicios_aerobicos(Usuario, NovoUsuario) :-
         writeln("Opção inválida."),
         submenu_exercicios_aerobicos(Usuario)
     ).
-
